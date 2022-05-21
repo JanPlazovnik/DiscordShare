02a5fb4d80b670566af2384cfbc2c4fc87a755a8
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-05-21 20:23:15 +0200

    Reformatted code

--- pages/api/user/[id]/index.ts
+++ pages/api/user/[id]/index.ts
@@ -1,40 +1,28 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from 'axios'
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export interface DiscordUser {
-  id: string;
-  username: string;
-  avatar: string;
-  discriminator: string;
-  public_flags: number;
-  banner?: string;
-  banner_color?: string;
-  accent_color?: string;
-}
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  const id = req.query.id
-  if (!id) {
-    return res.status(400).json({
-      status: 400,
-      message: "Bad request - User ID was not given."
-    })
-  }
-
-  try {
-    const { data } = await axios.get(`https://discordapp.com/api/users/${id}`, { headers: { "Authorization": `Bot ${process.env.BOT_TOKEN}` } })
-    return res.status(200).json(data)
-  } catch (e: any) {
-    if (e.response.status === 404) {
-      return res.status(404).json({
-        status: 404,
-        message: "Unknown User"
-      })
+    const id = req.query.id
+    if (!id) {
+        return res.status(400).json({
+            status: 400,
+            message: "Bad request - User ID was not given."
+        })
     }
-    return res.status(500).json({
-      status: 500,
-      message: "Internal Server Error"
-    })
-  }
+
+    try {
+        const { data } = await axios.get(`https://discordapp.com/api/users/${id}`, { headers: { "Authorization": `Bot ${process.env.BOT_TOKEN}` } })
+        return res.status(200).json(data)
+    } catch (e: any) {
+        if (e.response.status === 404) {
+            return res.status(404).json({
+                status: 404,
+                message: "Unknown User"
+            })
+        }
+        return res.status(500).json({
+            status: 500,
+            message: "Internal Server Error"
+        })
+    }
 }
