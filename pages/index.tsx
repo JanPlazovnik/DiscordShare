40404c8d5f2ab74fe61d1407449b536e45a8736e
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-05-21 19:59:01 +0200

    Added background image and reformatted the code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,43 +3,44 @@ import axios from "axios"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import ProfileCard from "../components/ProfileCard"
-import { DiscordUser } from "./api/user/[id]"
+import { DiscordUser } from '../types/discord';
+import background from '../public/discord_background.svg';
 
 export default function Home() {
-  const [user, setUser] = useState<DiscordUser|null>(null)
-  const router = useRouter()
-  const {id} = router.query
+    const [user, setUser] = useState<DiscordUser | null>(null);
+    const router = useRouter();
+    const { id } = router.query;
 
-  useEffect(() => {
-    if (!router.isReady) return
+    useEffect(() => {
+        if (!router.isReady) return;
 
-    async function fetchData() {
-      try {
-        const {data} = await axios.get(`/api/user/${id}`)
-        setUser(data)
-      } catch (e: any) {
-        console.error(e)
-      }
-    }
+        async function fetchData() {
+            try {
+                const { data } = await axios.get(`/api/user/${id}`);
+                setUser(data);
+            } catch (e: any) {
+                console.error(e);
+            }
+        }
 
-    fetchData()
-  }, [router.isReady])
+        fetchData();
+    }, [router.isReady]);
 
-  return (
-    <>
-      <Head>
-        <title>{ user ? user.username : "Discord Profile" }</title>
-      </Head>
-      <div className="wrapper">
-        <div className="box">
-          {
-            user
-            ? <ProfileCard user={user}/>
-            : <p style={{padding: "20px", margin: 0}}>Loading...</p>
-          }
-        </div>
-        <a href="https://github.com/JanPlazovnik/DiscordShare" className="footer-url">GitHub</a>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>{user ? user.username : "Discord Profile"}</title>
+            </Head>
+            <div className="wrapper" style={{backgroundImage: `url(${background.src})`}}>
+                <div className="box">
+                    {
+                        user
+                            ? <ProfileCard user={user} />
+                            : <p style={{ padding: "20px", margin: 0 }}>Loading...</p>
+                    }
+                </div>
+                <a href="https://github.com/JanPlazovnik/DiscordShare" className="footer-url">GitHub</a>
+            </div>
+        </>
+    );
 }
